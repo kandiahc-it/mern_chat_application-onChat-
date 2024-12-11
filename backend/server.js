f01f7b3c247cc7780f92240bdf9a69b149b39fc7
Kandiah C <kandiahc.it2023@citchennai.net>
2024-12-11 12:29:26 +0530

    deployement code

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import authRoutes from './router/authroutes.js';
 import messagesRoutes from './router/messagesroutes.js';
 import userRoutes from './router/usersroutes.js'
-
+import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
@@ -14,7 +14,7 @@ dotenv.config();
 
 // const app=express();
 const PORT =process.env.PORT || 5000;
-
+const __dirname=path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 //app.use(express.urlencoded({ extended: true }));
@@ -22,9 +22,11 @@ app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messagesRoutes);
 app.use('/api/users',userRoutes)
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-
-
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+});
 
 
 server.listen(PORT,()=>{
