5444742a1f4f06b4df167c304c507de00c8c73ab
Kandiah C <kandiahc.it2023@citchennai.net>
2024-12-11 11:58:46 +0530

    notification sound

--- frontend/src/components/hooks/useListenMessages.js
+++ frontend/src/components/hooks/useListenMessages.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import {useSocketContext} from '../../context/SocketContext'
 import useConversation from '../../zustand/useConversation';
-
+import notificstionSound from '../../assets/notification.mp3'
 const useListenMessages = () => {
  const{socket} = useSocketContext();
  const {messages,setMessages}=useConversation();
@@ -9,6 +9,8 @@ const useListenMessages = () => {
  useEffect(()=>{
     socket?.on("newMessage",(newMessage)=>{
         newMessage.shouldShake=true;
+        const sound=new Audio(notificstionSound);
+        sound.play();
         setMessages([...messages,newMessage])
     })
     return () => socket?.off("newMessage")
